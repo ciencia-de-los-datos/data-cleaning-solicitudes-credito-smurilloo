6d1aa4680cf7b3d0d20be1780dfa308ca6e006c3
smurilloo <143961450+smurilloo@users.noreply.github.com>
2023-10-18 18:50:28 -0500

    Esta es mi solución al taller 5
    Esta es mi solución al taller 5 de limpieza de datos

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,38 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
+    # Leer el archivo CSV
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    
+    # Eliminar filas con valores faltantes
+    df = df.dropna()
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    # Eliminar comas, _, signo de dolar y espacios en blanco
+    df = df.apply(lambda x: x.astype(str).str.replace("_", "-").str.replace("-", " ").str.replace("$", "").str.replace(",", ""))
 
-    #
-    # Inserte su código aquí
-    #
+    # Convertir todas las columnas de texto a minusculas
+    df = df.apply(lambda x: x.astype(str).str.lower().str.strip())
+    
+
+    # Limpiar columna fecha_de_beneficio
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='mixed', dayfirst=True)
+
+    # Convertir a float columna monto_del_credito
+    df['monto_del_credito'] = df['monto_del_credito'].astype(float)
+    
+    # Eliminar registros duplicados
+    df = df.drop_duplicates()
+
 
     return df
+
+
+
+
+# Llamar a la función para limpiar los datos
+clean_data()
+
+# Verificar el resultado
+print(clean_data().head())  # Imprimir las primeras filas del DataFrame limpio
+print(clean_data().sexo.value_counts().to_list())
